bca6a404623e06c96c219d1937c5dd160475cf90
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-06-04 10:33:41 +0530

    seln sort init

---
+++ Sorting/01_selection_sort.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+void selectionSort(int arr[],int n){
+    
+    for(int i=0;i<=n;i++){
+        int min=arr[i];    
+        int minIndex=i;   
+        for(int j=i;j<=n-1;j++){
+            if(min>=arr[j]){
+                min=arr[j];
+                minIndex=j;
+            }
+        }
+        int temp=arr[i];
+        arr[i]=min;
+        arr[minIndex]=temp;
+    }
+}
+
+void Display(int arr[],int n){
+    for(int i=0;i<=n-1;i++){
+    printf("%d\t",arr[i]);
+}
+}
+
+int main(){
+
+
+int arr[]={13,46,24,52,20,9,8,3,2,1,3,3,2};
+selectionSort(arr,sizeof(arr)/4);
+Display(arr,sizeof(arr)/4);
+
+return 0;
+}
