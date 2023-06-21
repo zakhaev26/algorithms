86fd046836ca390264b49f8b09dbdf05044e7e03
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-06-22 02:17:18 +0530

    leetcode pr

--- C++ DSA/Leetcode/01_addsum.cpp
+++ C++ DSA/Leetcode/01_addsum.cpp
@@ -1,21 +1,43 @@
-// Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+// // Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
 
-// You must not use any built-in exponent function or operator.
+// // You must not use any built-in exponent function or operator.
 
-// For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+// // For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
  
 
-// Example 1:
+// // Example 1:
 
-// Input: x = 4
-// Output: 2
-// Explanation: The square root of 4 is 2, so we return 2.
+// // Input: x = 4
+// // Output: 2
+// // Explanation: The square root of 4 is 2, so we return 2.
 
-#include<math.h>
+// #include<math.h>
 
-class Solution {
-public:
-    int mySqrt(int x) {
-        return sqrt(x);
-    }
-};
+// class Solution {
+// public:
+//     int mySqrt(int x) {
+//         return sqrt(x);
+//     }
+// };
+async/await patterns
+promises
+Scope
+Dynamic scope
+Lexical scope
+Clousures 
+Hoisting
+Fetch/Axios API
+callbacks
+Try-catch/then-catch
+Chaining
+IIFE patterns
+Object oriented prototypes
+ES6 features (templated literals, destructing etc)
+Higher order functions (and operations like map, filter etc)
+Functional programming
+Generators and iterators
+Webscokets
+Memorization(caching)
+Web workers (offloading computation to background)
+Testing
+
