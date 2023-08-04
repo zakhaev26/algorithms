b09eb38f7fb16d93f6e8f18a1d13154bf19299df
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-08-04 16:10:17 +0530

    problems solution and restructuring

---
+++ C++/Striver-SDE-Problems/Patterns/07.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<cctype>
+#include<string>
+using namespace std;
+int main(){
+
+for(int i =1;i<=5;i++){
+    for(int j = 5;j>i;j--){
+        cout<<" ";
+    }
+    for(int j = 1;j<=2*i-1;j++){
+        cout<<"*";
+    }
+    cout<<endl;
+}
+
+
+return 0;
+}
