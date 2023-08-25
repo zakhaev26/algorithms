31efcf009c8940979c1245f71dd4e3ab2cd1b8eb
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-08-25 20:11:41 +0530

    added DMA codes

--- C++/Basics/arri_iarr.cxx
+++ C++/Basics/arri_iarr.cxx
@@ -5,8 +5,20 @@ int main(){
 
 int arr[3] = {1,2,3};
 
-int i = 1;
-cout<<i[arr]<<endl;
+// int *i = &arr[0];
+// cout<<sizeof(i);
+// cout<<sizeof(i);
+// cout<<i[arr]<<endl;
+// cout<< sizeof(long int ) <<endl;
+// cout<< sizeof( int ) <<endl;
+// cout<< sizeof( long long int ) <<endl;
+// cout<< sizeof(&arr[0]) <<endl;
+
+int p = 3;
+int *q = &p;
+int **r = &q;
+cout<<*(*r)<<endl; 
+
 
 return 0;
 }
