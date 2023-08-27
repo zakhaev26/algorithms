4daf47cb7d0e2fa171983a15b0fb1046aad4af2d
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-08-28 02:37:46 +0530

    added problem codefiles

---
+++ C++/practice/mergesort.cxx
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void mergeSort(vector<int> &arr,int low,int high) {
+    if(low>=high)
+}
+
+int main(){
+
+vector<int> arr = {1,5,4,2,3};
+
+mergeSort(arr,0,arr.size());
+
+
+
+return 0;
+}
