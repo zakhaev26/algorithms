953ba855a0f9037ad9d79f4355d9201f10ab3f4f
zakhaev26 <b422056@iiit-bh.ac.in>
2024-01-08 20:40:51 +0530

    206

---
+++ C++/Leetchod/test.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data =data;
+        this->next=NULL;
+    }
+};
+
+
+
+
+int main() {
+
+    
+
+    return 0;
+}
