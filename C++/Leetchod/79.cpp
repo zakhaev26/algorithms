217450037d43a0ecc2534c2f8c751fb73fdff083
zakhaev26 <b422056@iiit-bh.ac.in>
2024-01-23 11:48:26 +0530

    adds leetcode probs

--- C++/Leetchod/79.cpp
+++ C++/Leetchod/79.cpp
@@ -1,26 +1,63 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
-
-class Solution {
+// TODO : Optimize this code by changing the matrix elements to # .That woould reduce the need of Set and reduce TC
+class Solution
+{
 public:
+    bool canMove(int x, int y, int border_x, int border_y, set<pair<int, int>> &path)
+    {
+        return x <= border_x && y <= border_y && x >= 0 && y >= 0 && path.find({x, y}) == path.end();
+    }
 
-    void solve(vector<vector<char>>& board , string word,int i ,int j) {
+    bool solve(vector<vector<char>> &board, string &word, int x, int y, int i, set<pair<int, int>> &path)
+    {       
+        if (word.size() == i)
+            return true;
 
-        char ch = board[i][j];
+        // dfs failed
+        if (!canMove(x, y, board.size() -1 , board[0].size() -1, path))
+            return false;
 
-        for(auto it :board)
-            for(auto jt :it) {
+        // char doesnt match
+        if (board[x][y] != word[i])
+            return false;
 
-                if(jt == ch) {
+        // insert in set
+        path.insert(make_pair(x, y));
 
-                    if(canMove())
-                }
+        // 4 dfs calls
+        if (solve(board, word, x + 1, y, i + 1, path) || solve(board, word, x - 1, y, i + 1, path) || solve(board, word, x, y + 1, i + 1, path) || solve(board, word, x, y - 1, i + 1, path))
+            return true;
+
+        path.erase(make_pair(x,y));
+
+        // if not found , err
+        return false;
+    }
+
+    bool exist(vector<vector<char>> &board, string word)
+    {
+        set<pair<int, int>> path;
+        for(int i = 0;i< board.size();i++) {
+            for(int j = 0;j<board[0].size();j++){
+                if(solve(board, word, i,j,0,path)) return true;
             }
-    }
+        } 
 
-    bool exist(vector<vector<char>>& board, string word) {
-
+        return false;
     }
-}; 
+};
+
+
+int main() {
+
+    Solution *s = new Solution();
+    vector<vector<char>> board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+    string word = "SEE";
+    cout<<boolalpha;
+    cout<<s->exist(board,word);
+
+    return 0;
+}
