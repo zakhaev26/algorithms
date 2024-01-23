e320b484f911cf3e9af7a909d713044826263d3c
zakhaev26 <b422056@iiit-bh.ac.in>
2024-01-23 14:37:37 +0530

    adds leetcode qs

---
+++ C++/practice/snail_window.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void slidingWindow(string s ) {
+
+    int start = 0;
+    int len = 0;
+
+
+    set<char> st;
+
+    for(int i = 0;i<s.size();i++) {
+
+        if(st.count(s[i]) > 0) {
+            st.erase(s[start]);
+            start++;
+        }
+
+        st.insert(s[i]);
+    }
+
+
+}
+
+int main() {
+
+
+    return 0;
+}
---
+++ C++/practice/wtf.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+
+
+    int x = 10;
+    cout<<"ongoing...";
+    while(x-->0) {
+        cout << x<<"\n";
+    }
+
+    return 0;
+}
