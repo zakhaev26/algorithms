9f3b8f799df486316db50676ce463cf62c2a7e7d
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2024-12-31 23:25:24 +0530

    chore: move v2/* to root

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# algori
+# algorithms
 Algorithms repo
 
 
--- v2/sandbox/a2z/greedy/largest_permutation.py
+++
@@ -1,8 +0,0 @@
-# def Solution(arr:list[int],k):
-#     hmap:dict = {}
-    
-#     for e,i in arr:
-#         hmap[e] = i
-        
-#     for x in arr:
-#         if(hmap.get(x) != None):
